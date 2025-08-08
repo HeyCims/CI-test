6f9640fb53673395571ab9a42494b76a1ea475a8
IniMaret <bismaesa911@gmail.com>
2025-08-08 11:09:50 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.post('greet', (req, res) => {
+    const name = req.body.name || 'guest';
+    res.json({ message: `Hello, ${name}!` });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http:localhost:${port}`);
+});
